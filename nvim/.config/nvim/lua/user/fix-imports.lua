c080c498953ff76afff94227b1d16770e2cda328
Ryan Steiger <ryan.steiger@protonmail.com>
2026-05-09 09:13:10 +0200

    -adding new nvim setup with git submodule

--- nvim/.config/nvim/lua/user/fix-imports.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set('n', '<leader>fi', function()
-  vim.lsp.buf.code_action {
-    filter = function(action)
-      return action.kind and action.kind:match 'source.organizeImports'
-    end,
-    apply = true, -- Automatically apply the first match
-  }
-end, { noremap = true, silent = true })
--- nvim/.config/nvim/lua/user/formatting.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<leader>f', function()
-  vim.lsp.buf.format()
-end, { noremap = true, silent = true })
